8ce7624d8ef17663075acd12106ce3f6df915460
Sid1279 <siddharth.n.viswanath@gmail.com>
2023-08-04 14:01:30 -0400

    setup ide, added gitignore

--- test_stepper/test_stepper.ino
+++ test_stepper/test_stepper.ino
@@ -1,34 +1,9 @@
-int E = 13;
-int D = 12;
-int C = 11;
-int DP = 10;
-int B = 9;
-int A = 8;
-int F = 7;
-int G = 6;
-
-void one()
-{
-  digitalWrite(E,HIGH);
-  digitalWrite(D,LOW);
-  digitalWrite(C,LOW);
-  digitalWrite(DP,LOW);
-  digitalWrite(B,LOW);
-  digitalWrite(A,LOW);
-  digitalWrite(F,HIGH);
-  digitalWrite(G,LOW);
-}
-
 void setup () {
-  pinMode (E, OUTPUT);
-  pinMode (D, OUTPUT);
-  pinMode (C, OUTPUT);
-  pinMode (DP, OUTPUT);
-  pinMode (B, OUTPUT);
-  pinMode (A, OUTPUT);
-  pinMode (F, OUTPUT);
-  pinMode (G, OUTPUT);
+  pinMode (13, OUTPUT);
 }
 void loop() {
-    one();
+  digitalWrite (13, HIGH);
+  delay (1000);
+  digitalWrite (13, LOW);
+  delay (1000);
 }
