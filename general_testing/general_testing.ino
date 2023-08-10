53f46bbb752c3e931ee8bf9321743b699be628e3
Sid1279 <siddharth.n.viswanath@gmail.com>
2023-08-10 14:00:27 -0400

    custom code to control 3 step mots

--- general_testing/general_testing.ino
+++ general_testing/general_testing.ino
@@ -6,38 +6,48 @@
 #define ENABLE_PIN 12
 
 // motor 1
-#define DIR_1 10
-#define STEP_1 6
+#define DIR_1 12
+#define STEP_1 11
 
 // motor 2
 #define DIR_2 8
-#define STEP_2 5
+#define STEP_2 9
 
 // motor 3
-#define DIR_3 6
-#define STEP_3 9
+#define DIR_3 7
+#define STEP_3 6
 
-// motor 4
-#define DIR_4 4
-#define STEP_4 10
+// // motor 4
+// #define DIR_4 4
+// #define STEP_4 10
 
-// motor 5
-#define DIR_5 2
-#define STEP_5 3
+// // motor 5
+// #define DIR_5 2
+// #define STEP_5 3
 
-// motor 6
-#define DIR_6 15
-#define STEP_6 11
+// // motor 6
+// #define DIR_6 15
+// #define STEP_6 11
 
 StepperMotor motor_1(STEP_1, DIR_1, STEPPER_8_MICROSTEP);
+StepperMotor motor_2(STEP_2, DIR_2, STEPPER_8_MICROSTEP);
+StepperMotor motor_3(STEP_3, DIR_3, STEPPER_8_MICROSTEP);
 
 void setup (void)
 {
   Serial.begin(9600);
 
   motor_1.init();
-  motor_1.setPWMFrequency(20000); // 20kHz initially, redundant since default is 20kHz
-  motor_1.setPWMDutyCycle(50); // 50% speed initially, redundant since default is 50%
+  motor_1.setPWMFrequency(20000); // 20kHz initially
+  motor_1.setPWMDutyCycle(50); // 50% speed initially
+
+  motor_2.init();
+  motor_2.setPWMFrequency(20000); // 20kHz initially
+  motor_2.setPWMDutyCycle(50); // 50% speed initially
+
+  motor_3.init();
+  motor_3.setPWMFrequency(20000); // 20kHz initially
+  motor_3.setPWMDutyCycle(50); // 50% speed initially
 
   pinMode(ENABLE_PIN, OUTPUT);
 
@@ -52,6 +62,13 @@ static void main_task(void * pvParameters)
   {
     motor_1.driveSpeed(i);
     motor_1.run();
+
+    motor_2.driveSpeed(i);
+    motor_2.run();
+
+    motor_3.driveSpeed(i);
+    motor_3.run();
+    
     i += 0.1;
     if (i > 2)
       i = 0.1;
