782fa12616951c1888b573a70ab68b9b14842137
Sid1279 <siddharth.n.viswanath@gmail.com>
2023-08-17 00:16:24 -0400

    heap + other classes

--- modified_testing/motor.cpp
+++ modified_testing/motor.cpp
@@ -1,14 +1,15 @@
 #include "motor.hpp"
 
-StepperMotor::StepperMotor(int step_pin, int dir_pin, MICROSTEPS microsteps):
-  step(step_pin), dir(dir_pin)
+StepperMotor::StepperMotor(int step_pin, int dir_pin, MotorHeap *heap_in, MICROSTEPS microsteps):
+  step(step_pin), dir(dir_pin), heap(heap_in)
 {
   revoluteSteps = REVOLUTE_STEPS * microsteps;
   targetSpeed = 0;
   currentStep = 0;
   currentAngle = 0;
 
-  stepAngle = 1.0 / (double)revoluteSteps;
+  stepAngle = 2.0 * M_PI / (double)revoluteSteps;
+  heapNode.motor = this;
 }
 
 void StepperMotor::init(void)
@@ -60,10 +61,57 @@ void StepperMotor::stepMotor(void)
 
 void StepperMotor::run(void)
 {
-  currentSpeed = targetSpeed;
+  currentSpeed = targetSpeed; 
   stepMotor();
   
   double time_s = 1 / (currentSpeed / stepAngle);
   uint32_t delayTime = (time_s*1000000);
   delayMicroseconds(delayTime);
+}
+
+void StepperMotor::runHeap(void)
+{
+  currentSpeed = targetSpeed;
+  uint32_t interruptDelay{};
+
+  if (!digitalRead(step)){ // new cycle: step pin is low. calculate target speed
+    double time_s = (1/(currentSpeed / stepAngle)); // sec/step
+    lastStepTime = (time_s*1000000);
+    interruptDelay = MIN_PULSE / INTERRUPT_INTERVAL_US;
+    if (targetSpeed == 0)
+      return;
+    else if (targetSpeed > 0)
+      digitalWrite(dir, HIGH);
+    else
+      digitalWrite(dir, LOW);
+    digitalWrite(step, HIGH);
+  }
+  else{
+    interruptDelay = (lastStepTime - MIN_PULSE) / INTERRUPT_INTERVAL_US;
+    digitalWrite(step, LOW);
+  }
+
+  heapNode.count = interruptDelay;
+  heap->enqueue(&heapNode);
+}
+
+MotorHeap::MotorHeap():
+  size(0)
+{}
+
+int MotorHeap::getSize()
+{
+  return size;
+}
+
+bool MotorHeap::isEmpty()
+{
+  return size == 0;
+}
+
+void MotorHeap::print()
+{
+  for (int i = 0; i < size; i++)
+    Serial.print(heap[i]->count);
+  Serial.println();
 }
--- modified_testing/motor.hpp
+++ modified_testing/motor.hpp
@@ -8,7 +8,7 @@
 
 // TODO: determine expirmentally
 #define MIN_PULSE 20
-
+#define INTERRUPT_INTERVAL_US 2
 #define REVOLUTE_STEPS 200
 
 #define RAD2DEG (180 / M_PI)
@@ -44,13 +44,15 @@ class StepperMotor
     float currentAngle;
 
     stepper_control control_state;
-
-    SemaphoreHandle_t stepperMutex;
     
     void stepMotor(void);
+    uint32_t lastStepTime;
+
+    MotorHeap *heap;
+    HeapNode heapNode;
 
   public:
-    StepperMotor(int step_pin, int dir_pin, MICROSTEPS microsteps);
+    StepperMotor(int step_pin, int dir_pin, MotorHeap *heap_in, MICROSTEPS microsteps);
 
     void init(void);
 
@@ -64,7 +66,47 @@ class StepperMotor
     float getRPM(void) const;
 
     void run(void);
+    void runHeap(void);
+};
 
-};  
+struct HeapNode {
+    uint32_t count;
+    StepperMotor *motor;
+};
+
+class MotorHeap
+{
+  private:
+    HeapNode *heap[7];
+    int size;
+    void heapify(int i);
+    void swap(int i, int j);
+
+  public:
+    MotorHeap();
+ 
+    int getSize(void) const;
+    bool isEmpty(void) const;
+    void print(void) const; 
+
+    void dequeue(void);
+    void enqueue(HeapNode *node);
+};
+
+class MultiStepperDrive 
+{
+  private:
+    void drive(void);
+    int num_motors;
+    StepperMotor **motors;
+
+  public:
+    MultiStepperDrive(StepperMotor *motor, int size);
+
+    void init(void);
+
+    void driveSpeed(float *radPerSec, int size);
+    void driveSpeed(float radPerSec);
+};
 
 #endif
