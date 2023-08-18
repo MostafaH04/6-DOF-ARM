6649ef2e37f7c861d437647633ccb1ac728c9859
Sid1279 <siddharth.n.viswanath@gmail.com>
2023-08-17 22:24:19 -0400

    added both test cases

--- modified_testing/modified_testing.ino
+++ modified_testing/modified_testing.ino
@@ -97,8 +97,85 @@ void vTaskDelayMicro(unsigned int delayMicro)
   vTaskDelay(ticksToDelay);
 }
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // SID's VERSION WITH SERIAL MONITOR - controls each motor one by one, stops them one by one
 
-// // SID's VERSION WITH SERIAL MONITOR
+// #include <Arduino_FreeRTOS.h>
+// #include <semphr.h>
+// #include "motor.hpp"
+
+// // enable pin 
+// #define ENABLE_PIN 12
+
+// // Define the number of motors
+// #define NUM_MOTORS 6
+
+// // Array of motor step and dir pins
+// int stepPins[NUM_MOTORS] = {11, 9, 7, 5, 3, 14}; // Update with your step pin numbers
+// int dirPins[NUM_MOTORS] = {10, 8, 6, 4, 2, 15}; // Update with your dir pin numbers
+
+// MotorHeap motorHeap;
+// StepperMotor *motors[NUM_MOTORS];
+
+// MultiStepperDrive stepperDriver(motors);
+
+// void setup(void)
+// {
+//   Serial.begin(9600);
+//   pinMode(ENABLE_PIN, OUTPUT);
+
+//   for (int i = 0; i < NUM_MOTORS; i++)
+//   {
+//     motors[i] = new StepperMotor(stepPins[i], dirPins[i], &motorHeap, STEPPER_8_MICROSTEP);
+//     motors[i]->init();
+//   }
+
+//   xTaskCreate(main_task, "Main Task", 200, NULL, 3, NULL);
+// }
+
+// static void main_task(void *pvParameters)
+// {
+//   digitalWrite(ENABLE_PIN, HIGH);
+
+//   for (int i = 0; i < NUM_MOTORS; i++)
+//   {
+//     float speed = 2000;
+
+//     Serial.print("Setting speed to motor ");
+//     Serial.print(i);
+//     Serial.print(": ");
+//     Serial.println(speed);
+
+//     motors[i]->driveSpeed(speed);
+
+//     vTaskDelay(3000 / portTICK_PERIOD_MS); // Delay for 3 seconds
+
+//     motors[i]->driveSpeed(0); // Stop the motor
+
+//     Serial.print("Motor ");
+//     Serial.print(i);
+//     Serial.println(" stopped running.");
+//   }
+
+//   while (1)
+//   {
+//     vTaskDelay(1000); // Keep the task running
+//   }
+// }
+
+// void loop(void)
+// {
+// }
+
+// void vTaskDelayMicro(unsigned int delayMicro)
+// {
+//   TickType_t ticksToDelay = delayMicro * configTICK_RATE_HZ / 1000000;
+
+//   vTaskDelay(ticksToDelay);
+// }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+// // SID's VERSION WITH SERIAL MONITOR - control all 6 motors at once, sets them to 2000 for 5s, stops
 
 // #include <Arduino_FreeRTOS.h>
 // #include <semphr.h>
