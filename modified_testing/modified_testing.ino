c794d07162efa57c9b26e92f41cd6e99a15f8432
Sid1279 <siddharth.n.viswanath@gmail.com>
2023-08-17 22:21:58 -0400

    added sid's test case :nerd:

--- modified_testing/modified_testing.ino
+++ modified_testing/modified_testing.ino
@@ -36,6 +36,12 @@ MultiStepperDrive stepperDriver(motors);
 
 ISR(TIMER1_COMPA_vect)
 {
+  // debug statements
+  Serial.println("Interrupt occurred"); // check if interrupt occurred
+
+  Serial.print("Motor Heap Size: ");
+  Serial.println(motorHeap.getSize());  
+
   TCNT1 = timer1_compare_match;
 
   motorHeap.decrement();
@@ -43,6 +49,9 @@ ISR(TIMER1_COMPA_vect)
   // check which motor is right now
   if(!motorHeap.isEmpty())
   {
+    Serial.println("Motors in Queue:");
+    motorHeap.print();
+
     while(motorHeap.top() == 0)
     {
       HeapNode* current = motorHeap.dequeue();
@@ -68,6 +77,8 @@ static void main_task(void * pvParameters)
   for (;;)
   {
     motor_1.driveSpeed(i);
+    Serial.print("Setting speed to motor: ");
+    Serial.println(i);
     motor_1.run();
     i += 0.1;
     if (i > 2)
@@ -85,3 +96,85 @@ void vTaskDelayMicro(unsigned int delayMicro)
 
   vTaskDelay(ticksToDelay);
 }
+
+
+// // SID's VERSION WITH SERIAL MONITOR
+
+// #include <Arduino_FreeRTOS.h>
+// #include <semphr.h>
+// #include "motor.hpp"
+
+// // enable pin 
+// #define ENABLE_PIN 12
+
+// // Define the number of motors
+// #define NUM_MOTORS 6
+
+// // Array of motor step and dir pins
+// int stepPins[NUM_MOTORS] = {11, 9, 7, 5, 3, 14};
+// int dirPins[NUM_MOTORS] = {10, 8, 6, 4, 2, 15}; 
+
+// MotorHeap motorHeap;
+// StepperMotor *motors[NUM_MOTORS];
+
+// MultiStepperDrive stepperDriver(motors);
+
+// void setup(void)
+// {
+//   Serial.begin(9600);
+//   pinMode(ENABLE_PIN, OUTPUT);
+
+//   for (int i = 0; i < NUM_MOTORS; i++)
+//   {
+//     motors[i] = new StepperMotor(stepPins[i], dirPins[i], &motorHeap, STEPPER_8_MICROSTEP);
+//     motors[i]->init();
+//   }
+
+//   xTaskCreate(main_task, "Main Task", 200, NULL, 3, NULL);
+// }
+
+// static void main_task(void *pvParameters)
+// {
+//   digitalWrite(ENABLE_PIN, HIGH);
+
+//   for (int i = 0; i < NUM_MOTORS; i++)
+//   {
+//     float speed = 2000;
+
+//     Serial.print("Setting speed to motor ");
+//     Serial.print(i);
+//     Serial.print(": ");
+//     Serial.println(speed);
+
+//     motors[i]->driveSpeed(speed);
+//   }
+
+//   vTaskDelay(5000 / portTICK_PERIOD_MS); // Run motors at 2000 steps for 5 seconds
+
+//   for (int i = 0; i < NUM_MOTORS; i++)
+//   {
+//     motors[i]->driveSpeed(0); // Stop the motor
+
+//     Serial.print("Motor ");
+//     Serial.print(i);
+//     Serial.println(" stopped running.");
+//   }
+
+//   vTaskDelay(5000 / portTICK_PERIOD_MS); // Delay for 5 seconds before next iteration
+
+//   while (1)
+//   {
+//     vTaskDelay(1000); // Keep the task running
+//   }
+// }
+
+// void loop(void)
+// {
+// }
+
+// void vTaskDelayMicro(unsigned int delayMicro)
+// {
+//   TickType_t ticksToDelay = delayMicro * configTICK_RATE_HZ / 1000000;
+
+//   vTaskDelay(ticksToDelay);
+// }
