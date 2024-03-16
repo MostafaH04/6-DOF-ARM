88b45fc8dda969a8fa87bd180c3f0432d236be95
Siddharth Nema <siddharthnema04@gmail.com>
2024-03-16 12:45:32 -0400

    Updates

--- core/control.py
+++ core/control.py
@@ -1,1 +1,10 @@
-import pyserial
+import serial
+
+uart = serial.Serial(port="COM2", baudrate=9600, timeout=0.1)
+
+while True:
+  coords = input("Enter the Desired xyz coordinates: ")
+
+  uart.write()
+
+
