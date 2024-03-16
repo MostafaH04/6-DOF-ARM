87faa20a0903b48633b4367e4210c04c270701ab
Sid1279 <siddharth.n.viswanath@gmail.com>
2024-03-16 16:22:58 -0500

    AccelStepper integration

--- motor-control/motor-control.ino
+++ motor-control/motor-control.ino
@@ -1,12 +1,57 @@
-float angle_1;
-float angle_2;
-float angle_3;
-float angle_4;
-float angle_5;
-float angle_6;
+#include <AccelStepper.h>
+#include <MultiStepper.h>
+#include "positions.h"
+
+#define STEPPER1_DIR_PIN 2
+#define STEPPER1_STEP_PIN 3
+
+#define STEPPER2_DIR_PIN 4
+#define STEPPER2_STEP_PIN 5
+
+#define STEPPER3_DIR_PIN 6
+#define STEPPER3_STEP_PIN 7
+
+#define STEPPER4_DIR_PIN 8
+#define STEPPER4_STEP_PIN 9
+
+#define STEPPER5_DIR_PIN 10
+#define STEPPER5_STEP_PIN 11
+    
+#define STEPPER6_DIR_PIN 12
+#define STEPPER6_STEP_PIN 13
+
+AccelStepper stepper1(AccelStepper::DRIVER, STEPPER1_STEP_PIN, STEPPER1_DIR_PIN);
+AccelStepper stepper2(AccelStepper::DRIVER, STEPPER2_STEP_PIN, STEPPER2_DIR_PIN);
+AccelStepper stepper3(AccelStepper::DRIVER, STEPPER3_STEP_PIN, STEPPER3_DIR_PIN);
+AccelStepper stepper4(AccelStepper::DRIVER, STEPPER4_STEP_PIN, STEPPER4_DIR_PIN);
+AccelStepper stepper5(AccelStepper::DRIVER, STEPPER5_STEP_PIN, STEPPER5_DIR_PIN);
+AccelStepper stepper6(AccelStepper::DRIVER, STEPPER6_STEP_PIN, STEPPER6_DIR_PIN);
+
+MultiStepper steppers;
+
+long angle_1;
+long angle_2;
+long angle_3;
+long angle_4;
+long angle_5;
+long angle_6;
 
 void setup() {
   Serial.begin(115200);
+
+  stepper1.setMaxSpeed(100);
+  stepper2.setMaxSpeed(100);
+  stepper3.setMaxSpeed(100);
+  stepper4.setMaxSpeed(100);
+  stepper5.setMaxSpeed(100);
+  stepper6.setMaxSpeed(100);
+
+  steppers.addStepper(stepper1);
+  steppers.addStepper(stepper2);
+  steppers.addStepper(stepper3);
+  steppers.addStepper(stepper4);
+  steppers.addStepper(stepper5);
+  steppers.addStepper(stepper6);
 }
 
 String readUART() {
@@ -112,7 +157,16 @@ void display() {
 }
 
 void driveSteppers(){
-
+  Positions positions;
+  long motorAngle1 = (long)map(angle_1, 0, 1023, 0, 360);
+  long motorAngle2 = (long)map(angle_2, 0, 1023, 0, 360);
+  long motorAngle3 = (long)map(angle_3, 0, 1023, 0, 360);
+  long motorAngle4 = (long)map(angle_4, 0, 1023, 0, 360);
+  long motorAngle5 = (long)map(angle_5, 0, 1023, 0, 360);
+  long motorAngle6 = (long)map(angle_6, 0, 1023, 0, 360);
+  positions.setPositionsAngles(motorAngle1, motorAngle2, motorAngle3, motorAngle4, motorAngle5, motorAngle6);
+  steppers.moveTo(positions.getPositions());
+  steppers.run();
 }
 
 
@@ -120,5 +174,5 @@ void loop() {
   String jointAngles = readUART();
   split(jointAngles);
   display();
-  // driveSteppers();
+  driveSteppers();
 }
