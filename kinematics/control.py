9de1bb588187a6694ac7b6ce196cb0f8b571f7a2
Siddharth Nema <siddharthnema04@gmail.com>
2024-03-16 13:18:23 -0400

    Updates

--- kinematics/control.py
+++ kinematics/control.py
@@ -2,6 +2,8 @@ import serial
 import kinematics
 import matrixUtils
 
+uart = serial.Serial(port="COM2", baudrate=9600, timeout=0.1)
+
 kin = kinematics.Kinematics(3)
 mat_utils = matrixUtils.MatrixUtils()
 
@@ -19,13 +21,7 @@ jac_t = [[0] * 3 for _ in range(6)]
 AA_t = [[0] * 6 for _ in range(6)]
 A_tA = [[0] * 3 for _ in range(3)]
 pinv = [[0] * 6 for _ in range(3)]
-
 joint_angles_0 = [1.0, 2.5, 3]
-
-
-# uart = serial.Serial(port="COM2", baudrate=9600, timeout=0.1)
-# uart.write()
-
 transform = [[0] * 4 for _ in range(4)]
 
 while True:
@@ -44,4 +40,4 @@ while True:
   joint_angles = kin.inverse(transform, jac, pinv, jac_t, AA_t, A_tA, joint_angles_0, 0.01, 0.01, 50)
   mat_utils.print_matrix([joint_angles], 1, 3, "Joint angles")
 
-  print(type(joint_angles))
+  uart.write(" ".join(joint_angles))
