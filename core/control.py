45e3dd88760a133006ab5e37f64da3b4091a9f4a
Sid1279 <siddharth.n.viswanath@gmail.com>
2024-03-16 12:12:04 -0500

    working yay

--- core/control.py
+++
@@ -1,8 +0,0 @@
-import serial
-
-uart = serial.Serial(port="COM2", baudrate=9600, timeout=0.1)
-
-while True:
-  coords = input("Enter the Desired xyz coordinates: ")
-
-  uart.write()
---
+++ kinematics/control.py
@@ -0,0 +1,45 @@
+import serial
+import kinematics
+import matrixUtils
+
+kin = kinematics.Kinematics(3)
+mat_utils = matrixUtils.MatrixUtils()
+
+kin.add_joint_axis([0,  0,  1,  4,  0,  0])
+kin.add_joint_axis([0,  0,  0,  0,  1,  0])
+kin.add_joint_axis([0,  0, -1,  -6, 0,  -0.1])
+
+kin.add_initial_end_effector_pose([[-1, 0, 0, 0],
+                                    [0, 1, 0, 6],
+                                    [0, 0, -1, 2],
+                                    [0, 0, 0, 1]])
+
+jac = [[0] * 3 for _ in range(6)]
+jac_t = [[0] * 3 for _ in range(6)]
+AA_t = [[0] * 6 for _ in range(6)]
+A_tA = [[0] * 3 for _ in range(3)]
+pinv = [[0] * 6 for _ in range(3)]
+
+joint_angles_0 = [1.0, 2.5, 3]
+
+
+# uart = serial.Serial(port="COM2", baudrate=9600, timeout=0.1)
+# uart.write()
+
+transform = [[0] * 4 for _ in range(4)]
+
+while True:
+  for i in range(1, 5):
+    while True:
+      coords = input(f"Enter the desired transform (4x4 matrix - row {i}): ")
+      input_coords = coords.split(" ")  # array
+      if len(input_coords) != 4:
+        print("Invalid input. Please enter 4 coordinates.")
+        continue
+      for j in range(4):
+        transform[i-1][j] = float(input_coords[j])
+      break
+  
+  print("Desired transform: ", transform)
+  joint_angles = kin.inverse(transform, jac, pinv, jac_t, AA_t, A_tA, joint_angles_0, 0.01, 0.01, 50)
+  mat_utils.print_matrix([joint_angles], 1, 3, "Joint angles")
